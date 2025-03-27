64c42c46a49451e2dc699dbbfd8771735832e06f
fmechulam <fmechulam@frba.utn.edu.ar>
2025-03-27 01:21:19 +0000

    intento ej 2

---
+++ Ejercicio2.sh
@@ -0,0 +1,13 @@
+sumar() {
+    local suma=$(($1 + $2))
+    echo $suma
+}
+
+echo "ingrese un numero"
+read primero
+echo "ingrese otro numero"
+read segundo
+
+resultado=$(sumar $primero $segundo)
+
+echo "el resultado de hacer $primero + $segundo es $resultado"
